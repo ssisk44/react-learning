e858e782d2c62abbfa8f8760ef0cf3b425267cb1
Samuel Sisk <samuelsisk444@gmail.com>
2024-01-26 17:35:13 -0500

    ez mode no video needed :)

--- vid8-useState/src/App.jsx
+++ vid8-useState/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  let [count, setCount] = useState(0);
 
   return (
-    <>
-    </>
+    <div className='app-container'>
+      <h1 className='counter'>{count}</h1>
+      <div className='buttons-container'>
+        <button className='decrement-button button' onClick={() => setCount(count-=1)}>Decrement</button>
+        <button className='reset-button button' onClick={() => setCount(0)}>Reset</button>
+        <button className='increment-button button' onClick={() => setCount(count+=1)}>Increment</button>
+      </div>
+    </div>
   )
 }
 
